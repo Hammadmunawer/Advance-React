1ebee919c00a8186bde137b04390411abdeec595
practice <hammadmunawer2004@gmail.com>
2025-04-18 01:51:34 +0500

    make changes

---
+++ react-demo/src/App.jsx
@@ -0,0 +1,15 @@
+import Greeting from "./components/Greeting.jsx"
+import Add from "./components/Add.jsx";
+import Header from "./components/Header.jsx";
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+       <Header/>
+       
+        </div>
+  )
+}
+
+export default App
---
+++ react-demo/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+    <h1>Welcome to my website</h1>
+    <nav>
+    <a href="#">Home</a>
+    
+    <a href="#">About</a>
+     
+    <a href="#">contact</a>
+
+
+    </nav>
+    </div>
+  )
+}
+
+export default Header
